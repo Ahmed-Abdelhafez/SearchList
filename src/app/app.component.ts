b34d0ad72ca04b1ee117d1c97f1d3cbfffacf9ba
Ahmed Abdelhafez <a.emad23545@gmail.com>
2021-06-19 09:57:45 +0200

    search list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'learnAngular';
+export class AppComponent implements OnInit {
+  query: string
+  artists: any
+  currentArtist: any
+
+  showArtist(item: any) {
+    this.query = item.name
+    item.highlight = !item.highlight
+    this.currentArtist = item
+  }
+
+  constructor( private http: HttpClient) {
+    this.query = ''
+  }
+
+  ngOnInit(): void {
+    this.http.get<any>('../assets/data.json').subscribe(
+      data => {
+        this.artists = data
+      })
+  }
+
 }
